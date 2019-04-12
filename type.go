3ef4672a22c951fd84fc89b93426626868ae38c0
Oliver Curry <owcurry42@gmail.com>
2019-04-12 15:26:07 -0700

    extended type checker

--- type.go
+++ type.go
@@ -2,24 +2,38 @@ package main
 
 type TypeT interface {
 	typeDesc()
+	Name() string
 }
+
+// DefaultT represents the default type
+// it does not exist in the concrete grammar
+// some nodes will return it during the type check phase when the type returned will not be used
+type DefaultT struct {
+}
+
+func (dt DefaultT) typeDesc() {}
+func (dt DefaultT) Name()     { return "default" }
 
 type FloatT struct {
 }
 
-func (f FloatT) typeDesc() {}
+func (f FloatT) typeDesc()    {}
+func (f FloatT) Name() string { return "float" }
 
 type IntT struct {
 }
 
-func (i IntT) typeDesc() {}
+func (i IntT) typeDesc()    {}
+func (f IntT) Name() string { return "int" }
 
 type StringT struct {
 }
 
-func (s StringT) typeDesc() {}
+func (s StringT) typeDesc()    {}
+func (f StringT) Name() string { return "string" }
 
 type ErrorT struct {
 }
 
-func (e *ErrorT) typeDesc() {}
+func (e ErrorT) typeDesc()    {}
+func (f ErrorT) Name() string { return "error" }
--- type_checker.go
+++ type_checker.go
@@ -2,37 +2,56 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"reflect"
 )
 
-func TypeCheck(root Node) (TypeT, error) {
+func TypeCheck(root Node) (TypeT, []error) {
+
+	ec = new(errorCollector)
 
 	switch r := root.(type) {
 	case ChunkC:
 		return TypeCheck(r.Block)
 	case BlockC:
-		for i := 0; i < len(r.StatLst); i++ {
+		for _, s := range r.StatLst {
+			_, err := TypeCheck(s)
+			ec.add(err...)
 		}
+		return DefaultT{}, ec.errors
 	case DefC:
 		idT, err := TypeCheck(r.Id)
-		if err != nil {
-			return nil, err
-		}
+		ec.add(err...)
+
 		expT, err := TypeCheck(r.Exp)
-		if err != nil {
-			return nil, err
-		}
+		ec.add(err...)
 
 		if idT != expT {
-			return nil, errors.New("Type of identifier must match type of expression")
+			ec.add(fmt.Errorf("Type of identifier %q does not match type of expression %s", r.Id.Id, expT))
+			return ErrorT, ec.errors
 		}
 
-		return idT, nil
+		return idT, ec.err
 	case DefLst:
+		for _, d := range r.List {
+			_, err := TypeCheck(d)
+			ec.add(err...)
+		}
+		return DefaultT{}, ec.errors
 	case IdC:
-		return r.TypeId, nil
+		return r.TypeId, ec.errors
 	case IdLst:
+		for _, i := range r.List {
+			_, err := TypeCheck(i)
+			ec.add(err...)
+		}
+		return DefaultT{}, ec.errors
 	case ExpLst:
+		for _, e := range r.List {
+			_, err := TypeCheck(e)
+			ec.add(err...)
+		}
+		return DefaultT{}, ec.errors
 	case BinaryOpC:
 
 		switch r.Op {
@@ -44,40 +63,58 @@ func TypeCheck(root Node) (TypeT, error) {
 			fallthrough
 		case "-":
 			lhs, err := TypeCheck(r.Lhs)
-			if !isNumber(lhs) || err != nil {
+			ec.add(err...)
+			if !isNumber(lhs) {
+				ec.add(fmt.Errorf("operator: %q expected float or int got: %s", r.Op, lhs.Name()))
+				return ErrorT{}, ec.errors
 			}
 			rhs, err := TypeCheck(r.Rhs)
-			if !isNumber(rhs) || err != nil {
+			ec.add(err...)
+			if !isNumber(rhs) {
+				ec.add(fmt.Errorf("operator: %q expected float or int got: %s", r.Op, rhs.Name()))
+				return ErrorT{}, ec.errors
 			}
 			floatType := reflect.TypeOf(FloatT{})
 			if reflect.TypeOf(lhs) == floatType || reflect.TypeOf(rhs) == floatType {
-				return FloatT{}, nil
+				return FloatT{}, ec.errors
 			} else {
-				return IntT{}, nil
+				return IntT{}, ec.errors
 			}
 		case "/":
 			lhs, err := TypeCheck(r.Lhs)
-			if !isNumber(lhs) || err != nil {
+			ec.add(err...)
+			if !isNumber(lhs) {
+				ec.add(fmt.Errorf("operator: %q expected float or int got: %s", r.Op, lhs.Name()))
+				return ErrorT{}, ec.errors
 			}
 			rhs, err := TypeCheck(r.Rhs)
-			if !isNumber(rhs) || err != nil {
+			ec.add(err...)
+			if !isNumber(rhs) {
+				ec.add(fmt.Errorf("operator: %q expected float or int got: %s", r.Op, rhs.Name()))
+				return ErrorT{}, ec.errors
 			}
-			return FloatT{}, nil
+			return FloatT{}, ec.errors
 		case "//":
 			lhs, err := TypeCheck(r.Lhs)
-			if !isNumber(lhs) || err != nil {
+			ec.add(err...)
+			if !isNumber(lhs) {
+				ec.add(fmt.Errorf("operator: %q expected float or int got: %s", r.Op, lhs.Name()))
+				return ErrorT{}, ec.errors
 			}
 			rhs, err := TypeCheck(r.Rhs)
-			if !isNumber(rhs) || err != nil {
+			ec.add(err...)
+			if !isNumber(rhs) {
+				ec.add(fmt.Errorf("operator: %q expected float or int got: %s", r.Op, rhs.Name()))
+				return ErrorT{}, ec.errors
 			}
-			return IntT{}, nil
+			return IntT{}, ec.errors
 		}
 	case IntC:
-		return IntT{}, nil
+		return IntT{}, ec.errors
 	case FloatC:
-		return FloatT{}, nil
+		return FloatT{}, ec.errors
 	case StringC:
-		return StringT{}, nil
+		return StringT{}, ec.errors
 	}
 
 	panic("AST Node not implemented")
@@ -91,5 +128,17 @@ func isNumber(t TypeT) bool {
 		return true
 	default:
 		return false
+	}
+}
+
+type errorCollector struct {
+	errors []error
+}
+
+func (ec *errorCollector) add(errs ...error) {
+	for _, e := range errs {
+		if e != nil {
+			ec.errors = append(ec.errors, e)
+		}
 	}
 }
